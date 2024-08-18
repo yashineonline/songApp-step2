4286d404fc453d348aaac203e56ee148f806a8b0
yashineonline <yashineonline@gmail.com>
2024-08-18 14:19:21 +0300

    Build app and prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: '/ilahiAppDemo18Aug2024/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -10,4 +11,6 @@ export default defineConfig({
     }
   }
 })
+
+
 
